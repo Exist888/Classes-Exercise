c7cfe4f32359332c20460bb0d91946ae272b486c
Filip Herbst <filip_herbst@yahoo.com>
2024-08-22 11:37:36 -0500

    Completed Classes Exercise 1

---
+++ Classes/Car.cs
@@ -0,0 +1,8 @@
+﻿namespace Classes;
+
+public class Car
+{
+    public string Make { get; set; }  // Property - Make of Car
+    public string Model { get; set; }  // Property - Model of Car
+    public int Year { get; set; }  //Property - Year of Car
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myNewCar = new Car();
+
+            myNewCar.Make = "Subaru";
+            myNewCar.Model = "Outback Wilderness";
+            myNewCar.Year = 2024;
+            
+            Console.Write($"I just got a new car! It is a {myNewCar.Year} {myNewCar.Make} {myNewCar.Model}.");
         }
     }
 }
